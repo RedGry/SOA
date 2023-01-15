c9443c299ff6431f493c7f3e6ae026a83dcb23c7
RedGry <trottimus.2001@mail.ru>
2023-01-15 04:08:59 +0300

    add EJB and beans to slsb pool + fix logic

--- second-service/src/main/java/se/ifmo/ru/secondservice/service/api/AgencyService.java
+++
@@ -1,9 +0,0 @@
-package se.ifmo.ru.secondservice.service.api;
-
-import se.ifmo.ru.secondservice.service.model.Flat;
-
-public interface AgencyService {
-    Flat findFlatWithBalcony(boolean cheapest, boolean balcony);
-
-    long getMostExpensiveFlat(long id1, long id2, long id3);
-}
